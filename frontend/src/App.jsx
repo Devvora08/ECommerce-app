752984816d0bd41b7d9bd072694b1f1be53ea103
Devvora <devvora08@gmail.com>
2024-12-25 11:18:33 +0530

    changes made

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,7 +7,7 @@ import 'react-toastify/dist/ReactToastify.css'
 
 //import the pages here
 import Home from './pages/Home'
-import Collection from './pages/collection'
+import Collection from './pages/Collection'
 import About from './pages/About'
 import Contact from './pages/Contact'
 import Product from './pages/Product'
